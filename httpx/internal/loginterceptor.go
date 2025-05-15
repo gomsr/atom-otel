6f10f7d53dc8eb0b60cf920106def7cb9d347e96
alice52 <zzhang_xz@163.com>
2025-05-15 20:20:55 +0800

    chore(zap): split from atom-kit

---
+++ httpx/internal/loginterceptor.go
@@ -0,0 +1,35 @@
+package internal
+
+import (
+	"net/http"
+	"time"
+
+	logx "github.com/gomsr/atom-log"
+
+	"go.opentelemetry.io/otel/propagation"
+)
+
+func LogInterceptor(r *http.Request) (*http.Request, ResponseHandler) {
+	start := time.Now()
+	return r, func(resp *http.Response, err error) {
+		duration := time.Since(start)
+		if err != nil {
+			logger := logx.Logger.WithContext(r.Context()).WithDuration(duration)
+			logger.Errorf("[HTTP] %s %s - %v", r.Method, r.URL, err)
+			return
+		}
+
+		var tc propagation.TraceContext
+		ctx := tc.Extract(r.Context(), propagation.HeaderCarrier(resp.Header))
+		logger := logx.Logger.WithContext(ctx).WithDuration(duration)
+		if isOkResponse(resp.StatusCode) {
+			logger.Infof("[HTTP] %d - %s %s", resp.StatusCode, r.Method, r.URL)
+		} else {
+			logger.Errorf("[HTTP] %d - %s %s", resp.StatusCode, r.Method, r.URL)
+		}
+	}
+}
+
+func isOkResponse(code int) bool {
+	return code < http.StatusBadRequest
+}
---
+++ trace/trace_test.go
@@ -0,0 +1,34 @@
+package trace
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gomsr/atom-otel"
+
+	"github.com/stretchr/testify/assert"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	oteltrace "go.opentelemetry.io/otel/trace"
+)
+
+func TestSpanIDFromContext(t *testing.T) {
+	tracer := sdktrace.NewTracerProvider().Tracer("test")
+	ctx, span := tracer.Start(
+		context.Background(),
+		"foo",
+		oteltrace.WithSpanKind(oteltrace.SpanKindClient),
+		oteltrace.WithAttributes(semconv.HTTPClientAttributesFromHTTPRequest(httptest.NewRequest(http.MethodGet, "/", nil))...),
+	)
+	defer span.End()
+
+	assert.NotEmpty(t, tracing.TraceIDFromContext(ctx))
+	assert.NotEmpty(t, tracing.SpanIDFromContext(ctx))
+}
+
+func TestSpanIDFromContextEmpty(t *testing.T) {
+	assert.Empty(t, tracing.TraceIDFromContext(context.Background()))
+	assert.Empty(t, tracing.SpanIDFromContext(context.Background()))
+}
